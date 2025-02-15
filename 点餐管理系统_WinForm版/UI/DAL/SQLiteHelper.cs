1e10b302076cdd588a9f2799cb7695a506bf4ee3
陈序源 <chenjx0395@163.com>
2025-02-15 20:19:44 +0800

    SQLiteHelper工具类

---
+++ 点餐管理系统_WinForm版/UI/DAL/SQLiteHelper.cs
@@ -0,0 +1,95 @@
+﻿using System.Data;
+using System.Data.SQLite;
+
+namespace DAL
+{
+    /// <summary>
+    /// 封装对SQLite数据库的常用操作
+    /// </summary>
+    public static class SqLiteHelper
+    {
+        private static string _connStr = "Data Source = RMS.db";
+
+        /// <summary>
+        /// 获取 SQLiteDataReader 来读取数据
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static SQLiteDataReader ExecuteReader(string sql, params SQLiteParameter[] parameters)
+        {
+            SQLiteConnection conn = new SQLiteConnection(_connStr);
+            conn.Open();
+            using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+            {
+                cmd.Parameters.AddRange(parameters);
+                return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+
+        }
+
+        /// <summary>
+        /// 执行非查询语句
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns>影响的行数</returns>
+        public static int ExecuteNonQuery(string sql, params SQLiteParameter[] parameters)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(_connStr))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// 查询第一行第一列数据
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static object ExecuteScalar(string sql, params SQLiteParameter[] parameters)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(_connStr))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.Parameters.AddRange(parameters);
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 通过SQLiteDataAdapter填充DataTable
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <param name="parameters"></param>
+        /// <returns>内存数据库对象</returns>
+        public static DataTable ExecuteDataTable(string sql, params SQLiteParameter[] parameters)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(_connStr))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.Parameters.AddRange(parameters);
+                    using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+
+    }
+}
