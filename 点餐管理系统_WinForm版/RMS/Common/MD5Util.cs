848ef5446aa53db8c15c5448fc3fea0890302551
陈序源 <chenjx0395@163.com>
2025-02-16 13:53:59 +0800

    MD5加密工具类

---
+++ 点餐管理系统_WinForm版/RMS/Common/MD5Util.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Common
+{
+    public static class MD5Util
+    {
+        public static string GetMD5(string str)
+        {
+            var md5 = MD5.Create();
+            var data = md5.ComputeHash(Encoding.UTF8.GetBytes(str));
+            var sb = new StringBuilder();
+            foreach (var t in data)
+            {
+                sb.Append(t.ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
