08c2ad9c284129c60cfd80d9f6ae81d4d4d7bdf7
陈序源 <chenjx0395@163.com>
2025-02-22 15:58:20 +0800

    医院管理系统项目搭建

---
+++ HospitalManagementSystem/UI/Login.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace HospitalManagementSystem
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.lblUname = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.txtUname = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label10.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label10.Location = new System.Drawing.Point(156, 64);
+            this.label10.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(450, 48);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "阳光医院信息管理系统";
+            this.toolTip1.SetToolTip(this.label10, "当前登录界面");
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.lblPwd);
+            this.panel2.Controls.Add(this.lblUname);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.pictureBox5);
+            this.panel2.Controls.Add(this.txtPwd);
+            this.panel2.Controls.Add(this.txtUname);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Location = new System.Drawing.Point(74, 151);
+            this.panel2.Margin = new System.Windows.Forms.Padding(6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1174, 738);
+            this.panel2.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Coral;
+            this.button2.Font = new System.Drawing.Font("宋体", 12F);
+            this.button2.Location = new System.Drawing.Point(716, 501);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 96);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "退出";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_2);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("宋体", 12F);
+            this.button1.Location = new System.Drawing.Point(348, 501);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 96);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "登录";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblPwd.ForeColor = System.Drawing.Color.Red;
+            this.lblPwd.Location = new System.Drawing.Point(848, 368);
+            this.lblPwd.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(127, 33);
+            this.lblPwd.TabIndex = 12;
+            this.lblPwd.Text = "label12";
+            // 
+            // lblUname
+            // 
+            this.lblUname.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblUname.ForeColor = System.Drawing.Color.Red;
+            this.lblUname.Location = new System.Drawing.Point(848, 242);
+            this.lblUname.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblUname.Name = "lblUname";
+            this.lblUname.Size = new System.Drawing.Size(200, 38);
+            this.lblUname.TabIndex = 11;
+            this.lblUname.Text = "label11";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(14, 12);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 9;
+            this.pictureBox5.TabStop = false;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtPwd.ForeColor = System.Drawing.Color.Gray;
+            this.txtPwd.Location = new System.Drawing.Point(474, 358);
+            this.txtPwd.Margin = new System.Windows.Forms.Padding(6);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.PasswordChar = '*';
+            this.txtPwd.Size = new System.Drawing.Size(358, 44);
+            this.txtPwd.TabIndex = 1;
+            // 
+            // txtUname
+            // 
+            this.txtUname.BackColor = System.Drawing.SystemColors.Window;
+            this.txtUname.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtUname.ForeColor = System.Drawing.Color.Gray;
+            this.txtUname.Location = new System.Drawing.Point(476, 232);
+            this.txtUname.Margin = new System.Windows.Forms.Padding(6);
+            this.txtUname.Name = "txtUname";
+            this.txtUname.Size = new System.Drawing.Size(358, 44);
+            this.txtUname.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("新宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label9.ForeColor = System.Drawing.Color.Peru;
+            this.label9.Location = new System.Drawing.Point(276, 362);
+            this.label9.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(174, 38);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "密  码：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("新宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label8.Location = new System.Drawing.Point(278, 238);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(173, 38);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "用户名：";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1322, 1041);
+            this.Controls.Add(this.panel2);
+            this.Name = "Login";
+            this.Text = "登录";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblPwd;
+        private System.Windows.Forms.Label lblUname;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.TextBox txtUname;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+
+        #endregion
+    }
+}
---
+++ HospitalManagementSystem/UI/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace HospitalManagementSystem
+{
+    public partial class Login : Form
+    {
+
+        public Login()
+        {
+            this.InitializeComponent();
+            // 无边框
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.ControlBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            
+        }
+        
+        //退出事件
+        private void button2_Click_2(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
